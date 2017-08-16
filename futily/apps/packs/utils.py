f7a14bf707331d1839dd0b2d3efa4d48d59def86
Dan Gamble <dangamble89@gmail.com>
2017-08-16 14:53:53 +0100

    Attempt to fix color choices issue

--- futily/apps/packs/utils.py
+++ futily/apps/packs/utils.py
@@ -4,6 +4,13 @@ from ..players.models import Player
 def color_choices():
     colors = Player.objects.values_list('color', flat=True).order_by('color').distinct()
 
+    if colors:
+        return [
+            (x, x.capitalize().replace('_', ' ')) for x in colors
+        ]
+
     return [
-        (x, x.capitalize().replace('_', ' ')) for x in colors
+        ('bronze', 'Bronze'),
+        ('silver', 'Silver'),
+        ('gold', 'Gold'),
     ]
