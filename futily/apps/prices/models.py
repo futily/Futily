eb45a5febf0243bf0a383e4c2efd9e2d93481967
Dan Gamble <dangamble89@gmail.com>
2017-11-05 23:14:27 +0000

    Initial price work
    Set up base app
    Get prices pulling from Futbin / Futhead
    Add initial prices to cards
    
    Ref #18
    Ref #19
    Ref #92

---
+++ futily/apps/prices/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Price(models.Model):
+    market = models.CharField(max_length=255, choices=[
+        ('pc', 'PC'),
+        ('ps', 'Playstation'),
+        ('xb', 'Xbox'),
+    ])
+    player = models.ForeignKey('players.Player')
+    value = models.CharField(max_length=100)
+    source = models.CharField(max_length=255, choices=[
+        ('futbin', 'Futbin'),
+        ('futhead', 'Futhead'),
+        ('futwiz', 'Futwiz'),
+    ])
+    last_update = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return f'<{self.player}> {self.market}: {self.value}'
