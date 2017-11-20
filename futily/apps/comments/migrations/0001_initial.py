890cb7d06623740ff6cea80f999a8e46a559c430
Dan Gamble <dan@dangamble.co.uk>
2017-11-20 21:48:05 +0000

    Does this comment migration work?

--- futily/apps/comments/migrations/0001_initial.py
+++ futily/apps/comments/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('django_comments_xtd', '0005_auto_20170828_2123'),
+        ('django_comments_xtd', '0005_auto_20170920_1247'),
     ]
 
     operations = [
