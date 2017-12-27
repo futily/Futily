ff42b9bb7793f3f098aa326e0f3e3026daa3704b
Dan Gamble <dan@dangamble.co.uk>
2017-12-27 18:11:23 +0000

    Tweak prices getter

--- futily/apps/prices/management/commands/get_prices.py
+++ futily/apps/prices/management/commands/get_prices.py
@@ -10,75 +10,70 @@ from futily.apps.prices.models import Price
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        futbin_prices = self.scrape_futbin()
-        futhead_prices = self.scrape_futhead()
-        prices = futbin_prices + futhead_prices
-
-        Price.objects.bulk_create(prices)
-
-    def scrape_futhead(self):
-        url = 'http://www.futhead.com/prices/api/?year=18&id={}'
-        sources = Source.objects.all()
-
+        futhead_url = 'http://www.futhead.com/prices/api/?year=18&id={}'
+        futbin_url = 'https://www.futbin.com/18/playerPrices?player={}'
         prices = {
             'xbox': [],
             'ps': [],
             'pc': [],
         }
-
-        for player in Player.objects.filter(source__in=sources):
-            req = Request(url.format(player.ea_id), headers={'User-Agent': 'Mozilla/5.0'})
-            res = json.loads(urlopen(req).read())
-
-            for price in res[str(player.ea_id)]['xbLowFive']:
-                prices['xbox'].append(
-                    Price(
-                        market='xb',
-                        player=player,
-                        value=price,
-                        source='futhead',
-                        last_update=dateparser.parse(str(res[str(player.ea_id)]['xbTime']))
-                    )
-                )
-
-            for price in res[str(player.ea_id)]['psLowFive']:
-                prices['ps'].append(
-                    Price(
-                        market='ps',
-                        player=player,
-                        value=price,
-                        source='futhead',
-                        last_update=dateparser.parse(str(res[str(player.ea_id)]['psTime']))
-                    )
-                )
-
-        return [item for sublist in prices.values() for item in sublist]
-
-    def scrape_futbin(self):
-        url = 'https://www.futbin.com/18/playerPrices?player={}'
         sources = Source.objects.all()
 
-        prices = {
-            'xbox': [],
-            'ps': [],
-            'pc': [],
-        }
-
         for player in Player.objects.filter(source__in=sources):
-            req = Request(url.format(player.ea_id), headers={'User-Agent': 'Mozilla/5.0'})
-            res = json.loads(urlopen(req).read())
+            print(f'Scraping {player}')
+            futhead_req = Request(futhead_url.format(player.ea_id), headers={'User-Agent': 'Mozilla/5.0'})
+            futhead_res = json.loads(urlopen(futhead_req).read())
 
-            for console, obj in res[str(player.ea_id)]['prices'].items():
-                for key, value in obj.items():
-                    if 'LCPrice' in key:
-                        prices[console].append(
+            try:
+                for price in futhead_res[str(player.ea_id)]['xbLowFive']:
+                    if price > 0:
+                        prices['xbox'].append(
                             Price(
-                                market=console if console != 'xbox' else 'xb',
+                                market='xb',
                                 player=player,
-                                value=int(value.replace(',', '') if value else value),
-                                source='futbin',
-                                last_update=dateparser.parse(res[str(player.ea_id)]['prices'][console]['updated'])
+                                value=price,
+                                source='futhead',
+                                last_update=dateparser.parse(str(futhead_res[str(player.ea_id)]['xbTime']))
                             )
                         )
+            except Exception as e:
+                print(e)
 
-        return [item for sublist in prices.values() for item in sublist]
+            try:
+                for price in futhead_res[str(player.ea_id)]['psLowFive']:
+                    if price > 0:
+                        prices['ps'].append(
+                            Price(
+                                market='ps',
+                                player=player,
+                                value=price,
+                                source='futhead',
+                                last_update=dateparser.parse(str(futhead_res[str(player.ea_id)]['psTime']))
+                            )
+                        )
+            except Exception as e:
+                print(e)
+
+            futbin_req = Request(futbin_url.format(player.ea_id), headers={'User-Agent': 'Mozilla/5.0'})
+            futbin_res = json.loads(urlopen(futbin_req).read())
+            try:
+                for console, obj in futbin_res[str(player.ea_id)]['prices'].items():
+                    for key, value in obj.items():
+                        if 'LCPrice' in key:
+                            price = int(value.replace(',', '') if value else value)
+
+                            if price > 0:
+                                prices[console].append(
+                                    Price(
+                                        market=console if console != 'xbox' else 'xb',
+                                        player=player,
+                                        value=price,
+                                        source='futbin',
+                                        last_update=dateparser.parse(futbin_res[str(player.ea_id)]['prices'][console]['updated'])
+                                    )
+                                )
+            except Exception as e:
+                print(e)
+
+        prices = [item for sublist in prices.values() for item in sublist]
+        Price.objects.bulk_create(prices)
